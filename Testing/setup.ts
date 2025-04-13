8462728b373d0a9477e02813432b51ec8811462e
IamMohitGupta <72707911+IamMohitGupta@users.noreply.github.com>
2025-04-13 12:00:05 -0600

    Delete Testing/setup.ts

--- Testing/setup.ts
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom';
-import { server } from './server';
-
-// Establish API mocking before all tests
-beforeAll(() => server.listen());
-// Reset handlers after each test
-afterEach(() => server.resetHandlers());
-// Clean up after the tests are finished
-afterAll(() => server.close());
